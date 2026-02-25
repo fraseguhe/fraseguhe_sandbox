af72f6714cbb4bfea2f4398f86dd64ee010a9efc
fraseguhe <fraseguhe@gmail.com>
2026-02-25 16:18:12 -0500

    Commit

--- Ejercicios de práctica 1.java
+++ Ejercicios de práctica 1.java
@@ -1,9 +1,12 @@
-import java.util.scanner;
 public class Ejercicios_de_práctica_1 {
     public static void main(String[] args) {
-    int [] array1 = new int [7];
-    for (int i = 0; i < array1.length, i++);
-    {System.in.}
-    int [] array1 = new int [7];
+        int[] array1 = new int[7];
+        for (int i = 0; i < array1.length; i++) {
+            array1[i] = i; // initialize with example values
+        }
+
+        for (int i = 0; i < array1.length; i++) {
+            System.out.println(array1[i]);
+        }
     }
-    }
+}
