9bdc20f99edbcb9e7f78e4bc0f1f95b0aedf6b30
fraseguhe <fraseguhe@gmail.com>
2026-02-25 14:49:17 -0500

    commit

---
+++ Java.java
@@ -0,0 +1,14 @@
+public class Java {
+    /* COMENTARIO 
+    DE VARIAS LINEAS
+    */
+    // COMENTARIO DE UNA SOLA LINEA
+ public static void main(String[] args) {
+    // todo lo que escribas aquí se ejecuta
+        int i=1;
+        while (i<=10) {
+                System.out.print(i);
+                i++;
+        }
+ }
+}
