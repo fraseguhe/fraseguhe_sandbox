7d1c0f27174856c941f19a3941d777c65f0197a0
fraseguhe <fraseguhe@gmail.com>
2026-02-25 16:47:51 -0500

    commit

--- EjerciciosPractica1.java
+++ EjerciciosPractica1.java
@@ -5,23 +5,23 @@ public class EjerciciosPractica1 {
     // Se crean los 3 arreglos de tipo entero con 7 posiciones cada uno
     int array1, array2, array3 = new int [7], [7], [7];
     // Se establece un bucle para que el usuario pueda llenar el arreglo1 con los numeros
-    for (int i = 0; i >= array1.length, i++) {
+    for (int i = 0; i <= array1.length, i++) {
         Scanner leer = new Scanner(System.in);
-    System.out.print("(Introduce un número entero) array1 Valor" + (i+1) + ":");
-    array1 [i] = leer.nextInt();   
-    leer.close();
+        System.out.print("(Introduce un número entero) array1 Valor" + (i+1) + ":");
+        array1 [i] = leer.nextInt();   
+        leer.close();
     }
     // Se establece un bucle para que el usuario pueda llenar el arreglo2 con los numeros
-     for (int i = 0; i >= array2.length, i++) {
+     for (int i = 0; i <= array2.length, i++) {
         Scanner leer = new Scanner(System.in);
-    System.out.print("(Introduce un número entero) array2 Valor" + (i+1) + ":");
-    array2 [i] = leer.nextInt();   
-    leer.close();
+        System.out.print("(Introduce un número entero) array2 Valor" + (i+1) + ":");
+        array2 [i] = leer.nextInt();   
+        leer.close();
     }
     // Se establece un bucle para que el sistema pueda llenar el arreglo3 con la diferencia de los primeros 2 arreglos
-    for (int i = 0; i >= array3.length, i++) {
-        array3 [i] = (array1 [1] - array2 [i]);
+    for (int i = 0; i <= array3.length, i++) {
+        array3 [i] = (array1 [i] - array2 [i]);
+        System.out.print("[" + array3[i] + "]");
     }
-    System.out.println("El resultado de la resta de los 2 arreglos es:"+ array3);
     }
     }
