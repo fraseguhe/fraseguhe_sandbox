1e8550e3a858e4932da47da873d78d21e6e8ca6a
fraseguhe <fraseguhe@gmail.com>
2026-02-25 20:12:31 -0500

    Comit

--- EjerciciosPractica1.java
+++ EjerciciosPractica1.java
@@ -6,6 +6,8 @@ public class EjerciciosPractica1 {
     int []array1 = new int [7], array2 = new int [7], array3 = new int [7];
     // Se crea un scanner para extraer los numeros en la consola
     Scanner leer = new Scanner(System.in);
+    // Se crea una variable double del array3 para mostar el promedio de este arreglo
+    double promedioarray3 = 0;
     // Se pone un bucle para que el usuario pueda llenar el arreglo1 con los numeros
     for (int i = 0; i < array1.length; i++) {
                 System.out.println("(Introduce un numero entero) array1 Valor" + (i+1) + ":");
@@ -19,8 +21,9 @@ public class EjerciciosPractica1 {
     // Se pone un bucle para que el sistema pueda llenar el arreglo3 con la diferencia de los primeros 2 arreglos
     for (int i = 0; i <array3.length; i++) {
         array3 [i] = (array1 [i] - array2 [i]);
-        System.out.print("[" + array3[i] + "]");
+        System.out.println("[" + array3[i] + "]");
     }
+        System.out.println("El promedio del array3 es: " + (promedioarray3/array3.length));
     leer.close();
     }
     }
